fd636de4dbc0c3b9289a7d021e5bb74ddd5ecd8b
Ben Wheatley <contact@benwh.com>
2012-02-06 17:13:47 +0000

    Add new blank Android project for app development

---
+++ app/src/ncl/team22/languagetutor/LanguagetutorActivity.java
@@ -0,0 +1,13 @@
+package ncl.team22.languagetutor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LanguagetutorActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
