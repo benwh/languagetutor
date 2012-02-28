ce4ce0b3b04edb565f3e481c18e4b0948f9f77b8
Ben Wheatley <contact@benwh.com>
2012-02-28 18:11:53 +0000

    Replace hello word with buttons for activities

--- app/gen/ncl/team22/languagetutor/R.java
+++ app/gen/ncl/team22/languagetutor/R.java
@@ -13,6 +13,15 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int gamesbutton=0x7f060004;
+        public static final int optionsbutton=0x7f060001;
+        public static final int profilebutton=0x7f060000;
+        public static final int revisionbutton=0x7f060003;
+        public static final int statsbutton=0x7f060006;
+        public static final int testsbutton=0x7f060005;
+        public static final int tutorialsbutton=0x7f060002;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
@@ -20,7 +29,6 @@ public final class R {
         public static final int create=0x7f040000;
     }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int hello=0x7f050000;
+        public static final int app_name=0x7f050000;
     }
 }
