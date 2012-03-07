3b618034aaced42e45e685080ec27f6f5bb0e90e
Misha Newman <misha.j.n@gmail.com>
2012-03-07 03:25:57 +0000

    Added the login and create profile pages (although they still need work) and the skeleton for the profileManagerclass

--- app/gen/ncl/team22/languagetutor/R.java
+++ app/gen/ncl/team22/languagetutor/R.java
@@ -14,16 +14,27 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int gamesbutton=0x7f060004;
-        public static final int optionsbutton=0x7f060001;
-        public static final int profilebutton=0x7f060000;
-        public static final int revisionbutton=0x7f060003;
-        public static final int statsbutton=0x7f060006;
-        public static final int testsbutton=0x7f060005;
-        public static final int tutorialsbutton=0x7f060002;
+        public static final int confirm_password=0x7f060002;
+        public static final int create_password=0x7f060001;
+        public static final int create_profile_button=0x7f060005;
+        public static final int create_sa=0x7f060004;
+        public static final int create_sq=0x7f060003;
+        public static final int create_username=0x7f060000;
+        public static final int gamesbutton=0x7f06000d;
+        public static final int input_password=0x7f060007;
+        public static final int input_username=0x7f060006;
+        public static final int login_button=0x7f060008;
+        public static final int optionsbutton=0x7f06000a;
+        public static final int profilebutton=0x7f060009;
+        public static final int revisionbutton=0x7f06000c;
+        public static final int statsbutton=0x7f06000f;
+        public static final int testsbutton=0x7f06000e;
+        public static final int tutorialsbutton=0x7f06000b;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int create_profile_layout=0x7f030000;
+        public static final int login_layout=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class raw {
         public static final int create=0x7f040000;
