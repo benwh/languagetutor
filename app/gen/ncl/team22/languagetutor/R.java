607b77f05cb5b802d37b69eb6446665dd59184be
Ben Wheatley <contact@benwh.com>
2012-03-08 20:27:09 +0000

    Rename layout files to remove redundant naming

--- app/gen/ncl/team22/languagetutor/R.java
+++ app/gen/ncl/team22/languagetutor/R.java
@@ -32,9 +32,10 @@ public final class R {
         public static final int tutorialsbutton=0x7f06000b;
     }
     public static final class layout {
-        public static final int create_profile_layout=0x7f030000;
-        public static final int login_layout=0x7f030001;
-        public static final int main=0x7f030002;
+        public static final int create_profile=0x7f030000;
+        public static final int login=0x7f030001;
+        public static final int login_profile_list=0x7f030002;
+        public static final int main=0x7f030003;
     }
     public static final class raw {
         public static final int create=0x7f040000;
