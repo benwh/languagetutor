473ac81458734e21b0affe3b6e148306b41625a0
James Taylor <therelevance@live.co.uk>
2012-03-09 15:37:04 +0000

    Added set-up page for tests and corresponding xml file to define layout

---
+++ app/src/ncl/team22/languagetutor/test/Setup.java
@@ -0,0 +1,23 @@
+package ncl.team22.languagetutor.test;
+
+import ncl.team22.languagetutor.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Activity where test options/settings will be decided to launch new test
+ * @author james
+ */
+public class Setup extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.test_setup);
+		
+		//more stuff
+	}
+
+}
