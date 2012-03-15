bebb783b6f1b7caa9663389b99d87b4226b3f113
James Taylor <therelevance@live.co.uk>
2012-03-15 20:18:04 +0000

    Slight semantic error in testdata (fixing)

--- app/res/raw/testdata.sql
+++ app/res/raw/testdata.sql
@@ -5,6 +5,6 @@ INSERT INTO `langset` VALUES (1, 'Days of the Week', 1, 0, 1);
 INSERT INTO `langset` VALUES (2, 'Numbers 1-10', 1, 0, 1);
 INSERT INTO `langset` VALUES (3, 'Alphabet', 2, 0, 1);
 INSERT INTO `langset` VALUES (4, 'Food', 2, 0, 1);
-INSERT INTO `langset` VALUES (5, 'Months 1-10', 2, 0, 1);
+INSERT INTO `langset` VALUES (5, 'Months', 2, 0, 1);
 INSERT INTO `langset` VALUES (6, 'Numbers 11-50', 2, 0, 1);
 INSERT INTO `langset` VALUES (7, 'Verbs', 2, 0, 1);
