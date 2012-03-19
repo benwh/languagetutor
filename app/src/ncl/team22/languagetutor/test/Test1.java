be3b158d88029382b39d4ed28b2ee1a068d59926
James Taylor <therelevance@live.co.uk>
2012-03-19 15:06:31 +0000

    More test data, questions regarding join and structure for getting entities, needs discussing/looking at

--- app/src/ncl/team22/languagetutor/test/Test1.java
+++ app/src/ncl/team22/languagetutor/test/Test1.java
@@ -1,11 +1,15 @@
 package ncl.team22.languagetutor.test;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import ncl.team22.languagetutor.R;
+import ncl.team22.languagetutor.data.DatabaseAdapter;
 
 /**
  * Activity for first style of test, multiple choice questions
@@ -14,6 +18,9 @@ import ncl.team22.languagetutor.R;
  */
 public class Test1 extends Activity
 {
+	public static final String	TABLE_TOPIC	= "langset";
+	// public static final String TABLE_ENTITY_SET = "entity_set";
+	public static int			setId		= 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -33,5 +40,28 @@ public class Test1 extends Activity
 		// doing as well
 		final TextView question = (TextView) findViewById(R.id.multi_question);
 		question.setText("You picked a test on " + topicId);
+	}
+
+	// Think this could be needed in a class of it's own called Entity perhaps,
+	// similar to topics (putting stuff in a nicer data structure to work with?
+	public void getEntities(Context ctx, String topicId)
+	{
+		DatabaseAdapter sDba = new DatabaseAdapter(ctx);
+		SQLiteDatabase sDb = sDba.getWritableDatabase();
+
+		Cursor c = sDb.query(TABLE_TOPIC, new String[]
+		{"setID"}, "name = " + "?", new String[]
+		{topicId}, null, null, null);
+		c.moveToFirst();
+		// Set the id for next query
+		setId = c.getInt(0);
+
+		// NEED TO DO JOIN
+		// then return data based on set id of entities, further methods to
+		// break up or work with this data perhaps
+
+		sDba.close();
+
+		// return (if applicable)
 	}
 }
