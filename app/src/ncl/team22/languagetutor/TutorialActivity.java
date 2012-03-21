6a3f65bf6501838df76327901f707775f6a68b40
Ben Wheatley <contact@benwh.com>
2012-03-21 08:53:15 +0000

    Add basic structure of classes and layouts for tuts+rev
    revision_innerX.xml layouts are to be used via <include> in the main
    revision layout, for the different types of revision.

---
+++ app/src/ncl/team22/languagetutor/TutorialActivity.java
@@ -0,0 +1,16 @@
+package ncl.team22.languagetutor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TutorialActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}
