2767f827fb05ea4ce9a9a047ef2e9f89e0c404e3
Kyran laptop <kyranjackson1991@hotmail.co.uk>
2012-04-01 18:00:53 +0100

    made some changes to replaceqa and ReplacePassword
    also entered some functions into profile to work with replacepassword and
    replaceqa should now get all details from database if provided with
    profileID (needs working on) also still need to create functions to save
    to database

--- app/src/ncl/team22/languagetutor/profile/ReplacePassword.java
+++ app/src/ncl/team22/languagetutor/profile/ReplacePassword.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import ncl.team22.languagetutor.R;
 
@@ -15,44 +17,59 @@ public class ReplacePassword extends Activity
 	private String	inputAnswer;
 	private String	inPass;
 	private String	inConfirmPass;
+	private String	mssg;
+	private int		proID;
 
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.replacepassword);
 
-		// get question from db **
-		theQuestion = "This is the Question";
+		// get profileID??
+		proID = 1;
+
+		// get question and answer from db **
+		theQuestion = Profile.getSQ(proID);
 
 		// display question
 		final TextView textViewToChange = (TextView) findViewById(R.id.textQuestion1);
 		textViewToChange.setText(theQuestion);
 
-		// get input, and store in temp variables
-		inputAnswer = (String) findViewById(R.id.intextanswer).toString();
-		inPass = (String) findViewById(R.id.intextpass).toString();
-		inConfirmPass = (String) findViewById(R.id.intextconfirmpass1).toString();
-
+		// when the "ok" button is clicked
 		final Button doneButton = (Button) findViewById(R.id.donebutton);
 		doneButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v)
 			{
-				if (theQuestion == inputAnswer)
+
+				// get input from editText fields
+				inputAnswer = ((EditText) findViewById(R.id.intextanswer)).getText().toString();
+				inPass = ((EditText) findViewById(R.id.intextpass)).getText().toString();
+				inConfirmPass = ((EditText) findViewById(R.id.intextconfirmpass1)).getText().toString();
+
+				// should the new password be set? + set appropriate message
+				if (Profile.authenticateSA(proID, inputAnswer))
 				{
-					if (inPass == inConfirmPass)
+					if (inPass.equals(inConfirmPass))
 					{
 						// set password
+						mssg = "New Password saved";
 					}
 					else
 					{
-						// message "passwords are not the same"
+						mssg = "passwords are not the same";
 					}
 				}
 				else
 				{
-					// message "incorrect answer"
+					mssg = "incorrect answer";
 				}
+
+				// show message to user
+				Toast toast = Toast.makeText(getApplicationContext(), mssg, Toast.LENGTH_SHORT);
+				toast.show();
+
+				// close activity
 			}
 		});
 	}
--- app/src/ncl/team22/languagetutor/profile/ReplaceQA.java
+++ app/src/ncl/team22/languagetutor/profile/ReplaceQA.java
@@ -2,15 +2,66 @@ package ncl.team22.languagetutor.profile;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import ncl.team22.languagetutor.R;
 
 public class ReplaceQA extends Activity
 {
+	private String	inPass;
+	private String	inQ;
+	private String	inA;
+	private String	mssg;
+	private int		proID;
+
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.replaceqa);
 
+		// get profileID??
+		proID = 1;
+
+		// when the "ok" button is clicked
+		final Button doneButton = (Button) findViewById(R.id.button1);
+		doneButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v)
+			{
+
+				// get input from editText fields
+				inPass = ((EditText) findViewById(R.id.editText1)).getText().toString();
+				inQ = ((EditText) findViewById(R.id.editText2)).getText().toString();
+				inA = ((EditText) findViewById(R.id.editText3)).getText().toString();
+
+				// should the new password be set? + set appropriate message
+				if (Profile.authenticatePassword(proID, inPass))
+				{
+					if (!inQ.isEmpty() && !inA.isEmpty())
+					{
+						// set q + a
+						mssg = "New Question + Answer saved";
+					}
+					else
+					{
+						mssg = "Question and Answer Required";
+					}
+				}
+				else
+				{
+					mssg = "incorrect Password";
+				}
+
+				// show message to user
+				Toast toast = Toast.makeText(getApplicationContext(), mssg, Toast.LENGTH_SHORT);
+				toast.show();
+
+				// close activity
+
+			}
+		});
 	}
 }
