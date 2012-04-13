3a44dce62960dc80df6fdab02362f69cd1ba8b3c
Ben Wheatley <contact@benwh.com>
2012-04-13 02:39:20 +0100

    Add SQL exports of CSV files.
    DDL needs to be changed (image_asset, audio_asset should be boolean)
    before this is merged in to testdata.sql

---
+++ data/sets.csv.sql
@@ -0,0 +1,10 @@
+INSERT INTO `langset` VALUES (0, "Colours", 1, 0, 1);
+INSERT INTO `langset` VALUES (1, "Days of the Week", 1, 0, 1);
+INSERT INTO `langset` VALUES (2, "Numbers 1-10", 1, 0, 1);
+INSERT INTO `langset` VALUES (3, "Alphabet", 2, 0, 1);
+INSERT INTO `langset` VALUES (4, "Food", 2, 0, 1);
+INSERT INTO `langset` VALUES (5, "Months", 2, 0, 1);
+INSERT INTO `langset` VALUES (6, "Numbers 11-50", 2, 0, 1);
+INSERT INTO `langset` VALUES (7, "Clothing", 2, 0, 1);
+INSERT INTO `langset` VALUES (8, "Verbs", 3, 0, 1);
+INSERT INTO `langset` VALUES (9, "Male/Female", 3, 0, 1);
