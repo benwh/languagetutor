a31bc46c2859f84dd6eae768f1cce7a0f3f7f425
Misha Newman <misha.j.n@gmail.com>
2012-04-17 23:55:02 +0100

    Fixed a bug that kept closing the application

--- app/src/ncl/team22/languagetutor/LanguagetutorActivity.java
+++ app/src/ncl/team22/languagetutor/LanguagetutorActivity.java
@@ -135,8 +135,6 @@ public class LanguagetutorActivity extends Activity
 				e.apply();
 
 				startActivity(new Intent(LanguagetutorActivity.this, ncl.team22.languagetutor.profile.Login.class));
-				finish();
-
 				break;
 		}
 		return super.onMenuItemSelected(featureId, item);
