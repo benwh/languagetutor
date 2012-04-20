b9bb93e72b674240d6a3310cde73521d0774d904
James Taylor <therelevance@live.co.uk>
2012-04-20 13:08:44 +0100

    Clothing/verbs names were wrong way round in test data

--- app/res/raw/testdata.sql
+++ app/res/raw/testdata.sql
@@ -7,8 +7,8 @@ INSERT INTO `langset` VALUES (3, 'Alphabet', 2, 0, 1);
 INSERT INTO `langset` VALUES (4, 'Food', 2, 0, 1);
 INSERT INTO `langset` VALUES (5, 'Months', 2, 0, 1);
 INSERT INTO `langset` VALUES (6, 'Numbers 11-50', 2, 0, 1);
-INSERT INTO `langset` VALUES (7, 'Verbs', 2, 0, 1);
-INSERT INTO `langset` VALUES (8, 'Clothing', 3, 0, 1);
+INSERT INTO `langset` VALUES (7, 'Clothing', 2, 0, 1);
+INSERT INTO `langset` VALUES (8, 'Verbs', 3, 0, 1);
 INSERT INTO `langset` VALUES (9, 'Male/Female', 3, 0, 1);
 
 INSERT INTO `langentity` VALUES(0, 0, 0, "one", "uno", 0, 0);
