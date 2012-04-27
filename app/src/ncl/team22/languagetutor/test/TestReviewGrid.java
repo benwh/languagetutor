493202159f2d1dc363c5650b24081ee9d5d8e3b0
Ben Wheatley <contact@benwh.com>
2012-04-27 03:48:22 +0100

    Add button handler for return button in treview

--- app/src/ncl/team22/languagetutor/test/TestReviewGrid.java
+++ app/src/ncl/team22/languagetutor/test/TestReviewGrid.java
@@ -4,7 +4,10 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 
 import ncl.team22.languagetutor.R;
@@ -21,6 +24,15 @@ public class TestReviewGrid extends Activity
 		final ArrayList<String> review = TestData.getReviewTest();
 		GridView gridview = (GridView) findViewById(R.id.gridview);
 		gridview.setAdapter(new ArrayAdapter<String>(this, R.layout.test_review_grid_row, review));
+
+		Button returnButton = (Button) findViewById(R.id.menu_return_btn);
+		returnButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v)
+			{
+				finish();
+			}
+		});
 	}
 
 }
