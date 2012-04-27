50722fa73a219fad311822d554d6ffe7bd9161c8
James Taylor <therelevance@live.co.uk>
2012-04-27 10:49:34 +0100

    More comments, removed some unecessary code/comments

--- app/src/ncl/team22/languagetutor/test/Topics.java
+++ app/src/ncl/team22/languagetutor/test/Topics.java
@@ -35,15 +35,6 @@ public class Topics extends ListActivity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.test_topics);
 
-		/*
-		 * final ListView lv = getListView(); lv.setTextFilterEnabled(true);
-		 * final ArrayList<Topic> topics =
-		 * Topic.getTopics(LevelSelect.getLevel()); setListAdapter(new
-		 * ArrayAdapter<Topic>(this, R.layout.test_topics_list_row, topics));
-		 */
-
-		// TODO: Look into adding best score to list view (to right of topic
-		// name)
 		SimpleAdapter adapter = new SimpleAdapter(this, lv, R.layout.test_topics_list_row, new String[]
 		{"Topic", "Score"}, new int[]
 		{R.id.test_topics_list_row1, R.id.test_topics_list_row2});
@@ -51,20 +42,6 @@ public class Topics extends ListActivity
 		populateList();
 
 		setListAdapter(adapter);
-
-		/*
-		 * lv.setOnItemClickListener(new OnItemClickListener() {
-		 * @Override public void onItemClick(AdapterView<?> a, View v, int i,
-		 * long l) { try { // As the ListView is a list of 'Topic's can now just
-		 * pull // the selected Topic out and use it for the intent Topic
-		 * selectedTopic = (Topic) lv.getItemAtPosition(i); Intent intent = new
-		 * Intent(getApplicationContext(), Test.class); // Use the intentTopic
-		 * variable as the name of the extra, // which can be used across
-		 * multiple classes. selectedTopic // is added as an Intent extra, as
-		 * Topic is Serializable intent.putExtra(intentTopic, selectedTopic);
-		 * intent.setFlags(98); startActivity(intent); } catch (Exception e) {
-		 * Log.e(TAG, e.toString()); } } });
-		 */
 
 		final Button mix = (Button) findViewById(R.id.mixtest);
 
@@ -81,17 +58,15 @@ public class Topics extends ListActivity
 		});
 	}
 
+	/**
+	 * Populates the gridview with hashmap data values
+	 */
 	private void populateList()
 	{
 		for (int i = 0; i < topics.size(); i++)
 		{
 			HashMap<String, String> temp = new HashMap<String, String>();
 			temp.put("Topic", topics.get(i).name);
-			// temp.put("Score", "Best: "
-			// +
-			// Topic.getBestResultById(Topic.getLangsetIdByName(topics.get(i).name))
-			// + "%"); SARAH - CHANGED THIS TO JUST REMOVE "BEST" TO DISPLAY
-			// BETTER (look at test topic on phone)
 			temp.put("Score", Topic.getBestResultById(Topic.getLangsetIdByName(topics.get(i).name))
 					+ "%");
 			lv.add(temp);
